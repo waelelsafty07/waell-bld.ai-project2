548684212cf615e4ce301923c23fdb0c11f71fdc
waelismael <UGS.140589@ci.suez.edu.eg>
2022-08-31 12:56:43 +0200

    feat: Add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Courses from "./components/Courses/Courses";
+import NavBar from "./components/NavBar/NavBar";
 const App = () => {
-  return <Courses />;
+  return (
+    <>
+      <NavBar />
+      <Courses />;
+    </>
+  );
 };
 
 export default App;
